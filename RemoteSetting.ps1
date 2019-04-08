fc21a1c4666fcc749d23bf4d5f513da61c64944b
Sei <takasawa@w0w.co.jp>
2019-04-08 20:32:22 +0900

    RemoteStartProcessを追加。リモート先でアプリケーションを起動できるようになる。

--- RemoteSetting.ps1
+++ RemoteSetting.ps1
@@ -1,7 +1,11 @@
-﻿	
+﻿"リモート設定"	
+
 Enable-PSRemoting -SkipNetworkProfileCheck
 
-
 Set-Item WSMan:\localhost\Client\TrustedHosts * -Force
+
+netsh advfirewall firewall add rule name=PsExec dir=in action=allow localport=rpc-epmap protocol=tcp program= %windir%\system32\svchost.exe
+netsh advfirewall firewall add rule name=PsExec dir=in action=allow localport=445 protocol=tcp
+netsh advfirewall firewall add rule name=PsExec dir=in action=allow localport=rpc protocol=tcp program=%SystemRoot%\system32\services.exe
 
 pause
---
+++ RemoteStartProcess.ps1
@@ -0,0 +1,70 @@
+﻿"指定したアプリケーションを遠隔で起動します"
+
+$currentPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $currentPath
+
+# セッション開始
+$beginSessionPath = $currentPath + "\BeginSession.ps1"
+Write-Host "Begin Session path = " $beginSessionPath 
+#powershell $beginSessionPath
+
+# _Setting.txtを読み込む
+
+$settingPath = $currentPath + "\_Settings.csv"
+$settingCSV = Import-Csv $settingPath -Encoding Default
+$Process = ($settingCSV | Where-Object{$_.Key -eq "Process"})[0].Value
+
+Write-Host ("起動ファイルパス : " + $RunPath )
+
+
+# _SessionList.csvを読み込みます
+$sessionListPath = $currentPath + "\_SessionList.csv"
+$sessionListCSV = Import-Csv $sessionListPath -Encoding Default #セッションリスト
+
+# データ配列を作る
+$arrayIPs = @()
+$arrayUsers = @()
+$arrayPWs = @()
+$arrayFolders = @()
+$sessionListCSV | ForEach-Object{
+    $arrayIPs += $_.IP
+    $arrayUsers += $_.User
+    $arrayPWs += $_.PW
+    $arrayFolders += $_.Folder
+}
+
+$num = $arrayIPs.length
+
+"--------------------------------実行開始--------------------------------"
+
+# 実行
+if($Process.Length -gt 0){
+    for($i = 0; $i -lt $num; $i++){
+       $ip = $arrayIPs[$i]
+       $ping = Test-Connection $ip -Count 1 -Quiet
+       if($ping){
+           $strDataPath = "\\" + $ip + "\\" + $arrayFolders[$i] + "\\" + $Process
+           if(test-path $strDataPath){
+            Write-Host ($strDataPath + 'を実行します')
+            $ipname = "\\" +  $ip
+
+            PsExec.exe -i -d -s -u $arrayUsers[$i] -p $arrayPWs[$i] $ipname $strDataPath
+           }else{
+            Write-Host ($strDataPath + 'は存在しませんでした')
+           }
+        }else{
+          Write-Host ($ip + 'に接続できませんでした') 
+        }  
+    }
+
+}else{
+   "Error : _Setting.csvに適切なPathがありません"
+}
+
+# セッション終了
+$endSessionPath = $currentPath + "\\EndSession.ps1"
+#powershell $endSessionPath
+
+"--------------------------------実行終了--------------------------------"
+
+
